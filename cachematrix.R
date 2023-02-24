667680407c5294953dc041c4ca3f80c1c0ce3ccf
afk-Legacy <jbond3132@gmail.com>
2023-02-24 23:17:39 +0500

    Implement makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion can be computationally expensive when the 
+## matrix is very large. To prevent long processing times, we
+## can cache the result of inversion of smaller matrices to 
+## prevent recalculation, hence saving time
 
-## Write a short comment describing this function
+## This function creates a list which contains 
+## helper function for the matrix, which include:
+##  set the value of the matrix
+##  get the value of the matrix
+##  set the value of the inverted matrix
+##  get the value of the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invertedMatrix <- NULL
+    
+    set <- function(mat) {
+      x <<- mat
+      invertedMatrix <<- mat
+    }
+    
+    get <- function() x
+    
+    setInvertedMatrix <- function(invMat) invertedMatrix <<- invMat
+    
+    getInvertedMatrix <- function() invertedMatrix
+    
+    list(get=get, set=set, getInvertedMatrix=getInvertedMatrix, 
+         setInvertedMatrix=setInvertedMatrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInvertedMatrix
+    
+    if (!is.null(m)) {
+      message("Getting cached data....")
+      return(m)
+    }
+    
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInvertedMatrix(m)
+    
+    m
 }
